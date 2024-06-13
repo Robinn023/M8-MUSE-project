469c57d792949a3921b4ad5fc15f7a926389ebeb
Kjell06 <34952@ma-web.nl>
2024-06-13 14:37:27 +0200

    3d versie 1

--- js/app.js
+++ js/app.js
@@ -1,32 +1,64 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 const container = document.getElementById('container');
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(container.clientWidth, container.clientHeight);
 container.appendChild(renderer.domElement);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5, 5, 5).normalize();
-scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff, 30); 
+scene.add(ambientLight);
 
-const loader = new THREE.GLTFLoader();
-loader.load('/3dmodel/scene.gltf', function(gltf) {
-    scene.add(gltf.scene);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 20); 
+directionalLight.position.set(0, 10, 7.5); 
+scene.add(directionalLight);
+
+camera.position.z = 150; // Hier kan je de afstand mee aanpassen tussen het model en de camera
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; 
+controls.dampingFactor = 0.25; 
+controls.enableZoom = true; 
+
+const loader = new GLTFLoader();
+loader.load('../3dmodel/test_try_3d.glb', function (gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+
+    model.scale.set(5, 5, 5);
+    model.position.y = -100;
+    model.position.x = -0;
+    model.position.z = -0;
+
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        controls.update(); 
+
+        renderer.render(scene, camera);
+    }
+
     animate();
-}, undefined, function(error) {
+
+}, undefined, function (error) {
     console.error(error);
 });
 
-camera.position.z = 5;
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
+window.addEventListener('resize', function () {
+    camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(container.clientWidth, container.clientHeight);
 });
+
+/*npx vite
+
+Run code voor de terminal
+
+*/
